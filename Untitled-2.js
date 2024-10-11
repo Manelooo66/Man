78c991e19e92b7816badcf8927579039ff926d68
Manel Navarrete <manelitus6@gmail.com>
2024-10-11 18:32:27 +0200

    EXERCICIS OPERADORS

---
+++ Untitled-2.js
@@ -0,0 +1,14 @@
+// 4.3.-- Quins valors s'escriuran a la consola després d'executar aquest codi? // 
+
+let a = 5;
+let b = 18;
+let c = 15;
+let d = 22;
+a = a + 10;// 15
+b = b + 5 - c; // 8 
+c = c + 4 + b; //
+d = d + b + a;
+a = a + 1;
+b = b + c;
+c = b + c;
+d = b + b;
